d771919c0d27365119ad15de7b5572627422e5d3
Marcus Girolneto <marcus.girolneto@gmail.com>
2019-09-10 13:23:22 -0300

    Estoque na alteracao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
@@ -21,7 +21,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
     return;
   }
   const response = yield call(api.get, `/products/${id}`);
@@ -32,5 +32,20 @@ function* addToCart({ id }) {
   };
   yield put(addToCartSuccess(data));
 }
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
